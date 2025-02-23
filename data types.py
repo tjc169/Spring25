afdce44d3e528f3e3728302f289ced6c1c4b1a0d
tjc169 <tjc169@gmail.com>
2025-02-23 17:13:44 -0600

    Add files via upload
    data types assignment

---
+++ data types.py
@@ -0,0 +1,23 @@
+# This is a Python program to demonstrate different data types
+print("This my assignment for PE1 2.2: Understanding Data Types in Python")
+# Literal
+print("Literal:", 10)
+
+# Integer
+y = 45
+x = 10
+print("Integer:", x)
+
+# Float
+z = 37.50
+print("Float:", z)
+
+# String
+name = "Thomas"
+print("String:", name)
+
+# Boolean
+red = True
+blue = False
+print("Boolean:", red)
+print(name == "Michael")
