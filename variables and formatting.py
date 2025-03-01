982cacaf7b1ab2b6a11371f4e18e8f20e593687c
tjc169 <tjc169@gmail.com>
2025-03-01 08:24:28 -0600

    Add files via upload

--- variables and formatting.py
+++ variables and formatting.py
@@ -1,7 +1,20 @@
-var_1 = 2.3345
-var_2 = 2.24
-var_3 = "Tom"
+# kilograms values
+kg_value_1 = 25.4
+kg_value_2 = 95.6
+kg_value_3 = 182.1
+kg_value_4 = 225.3
 
-print(f"Hello, my name is {var_3}")
-print(f"We are formatting, var.1: {var_1:.2f}")
-print(f"{var_2:.1f}")
+# Conversion factor: Pounds (lb) = Kilograms (kg) * 2.20462
+conversion_factor = 2.20462
+
+# Calculate lbs
+lb_1 = kg_value_1 * conversion_factor
+lb_2 = kg_value_2 * conversion_factor
+lb_3 = kg_value_3 * conversion_factor
+lb_4 = kg_value_4 * conversion_factor
+
+# converted results
+print(f"{kg_value_1} kilograms is equal to {lb_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {lb_2:.2f} pounds.")
+print(f"{kg_value_3} kilograms is equal to {lb_3:.2f} pounds.")
+print(f"{kg_value_4} kilograms is equal to {lb_4:.2f} pounds.")
