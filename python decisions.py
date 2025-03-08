c4b31ff76bef36ad0d830aa92ed6924ef68556a7
Tom Cooper <tjc169@gmail.com>
2025-03-08 11:26:47 -0600

    decisions

---
+++ python decisions.py
@@ -0,0 +1,22 @@
+a = int(input("How old are you?  "))
+
+
+if a >= 16:
+    print("You are old enough to drive.")
+else:
+    print("You are not old enough to drive.")
+
+if a >= 18:
+    print("You are old enough to vote.")
+else:
+    print("You are not old enough to vote.")
+
+if a >= 21:
+    print("You are old enough to buy alcohol.")
+else:
+    print("You are not old enough to buy alcohol.")
+
+if a >= 65:
+    print("You are old enough to receive a senior discount.")
+else:
+    print("You are not old enough to receive a senior discount.")
