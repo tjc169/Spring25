ee4bc54dbcaf5c647d260857be576ff906b5b0ee
Tom Cooper <tjc169@gmail.com>
2025-03-17 16:29:24 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,10 @@
+# Input side length of square
 side_length = int(input(f"Enter the length of the sides of the square:"))
+
+# Input radius of circle
 radius = int(input(f"Enter the radius of the circle: "))
+
+# Use functions to determine areas
 
 
 def square(side):
@@ -10,5 +15,6 @@ def circle(radius):
     return 3.14 * radius
 
 
+# Print results
 print(f"\nThe area of the square is", square(side_length), "square units.")
 print(f"\nThe area of the circle is", circle(radius), "square units.")
