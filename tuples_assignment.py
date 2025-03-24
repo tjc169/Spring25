bf2f58de5c78404e7ea78fc7c86757a4a4bd3059
Tom Cooper <tjc169@gmail.com>
2025-03-23 19:18:25 -0500

    tuples classes

---
+++ tuples_assignment.py
@@ -0,0 +1,10 @@
+def main():
+    programming_classes = ("Intro to Python", "Advanced Python", "Database Essentials",
+                           "Website Development Basics", "Data Structures in Python", "Web Design Fundamentals")
+    for item in programming_classes:
+        print(item)
+    print("\nThere are", len(programming_classes),
+          "programming classes available at McHenry County College.")
+
+
+main()
